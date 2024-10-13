c1a546dbd8142330cd02c9ae0eaced383bbfa582
DerGiftzwerg156 <50719189+DerGiftzwerg156@users.noreply.github.com>
2024-10-13 23:46:15 +0200

    Initial Commit

---
+++ src/main/java/com/printer/fileque/FileQueApplication.java
@@ -0,0 +1,13 @@
+package com.printer.fileque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileQueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileQueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/printer/fileque/FileQueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printer.fileque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileQueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
